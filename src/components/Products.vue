b996eebecfefb9ecf60f0e44e692ef7e8a09bbae
vanessawa <81361169+vanessawa@users.noreply.github.com>
2021-10-19 22:06:47 +0200

    style: add grid for product-list

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,12 +1,17 @@
 <template>
   <article>
-    <div v-for="product in products" :key="product.id">
+    <div
+      class="prodDiv gridItem1"
+      v-for="product in products"
+      :key="product.id"
+    >
       <img class="prodPic" :src="`./assets/${product.id}.jpeg`" alt="gear" />
-      <p>{{ product.title }}</p>
-      <p>{{ product.manufacturer }}</p>
-      <p>{{ product.description }}</p>
-      <p>{{ product.netPrice }}</p>
-      <p>{{ product.grossPrice }}</p>
+      <p class="gridItem2">{{ product.title }}</p>
+      <p class="gridItem3">{{ product.manufacturer }}</p>
+      <p class="gridItem4">{{ product.description }}</p>
+      <p class="gridItem5">{{ product.netPrice }}</p>
+      <p class="gridItem6">{{ product.grossPrice }}</p>
+      <button>add to card</button>
     </div>
   </article>
 </template>
@@ -38,5 +43,52 @@ export default {
   height: 5rem;
   width: 5rem;
   background-size: contain;
+}
+
+.prodDiv {
+  border: 2px solid black;
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: 1fr 1fr;
+}
+
+.gridItem1 {
+  grid-column-start: 1;
+  grid-column-end: 2;
+}
+
+.gridItem2 {
+  grid-column-start: 2;
+  grid-column-end: 3;
+  justify-self: left;
+}
+
+.gridItem3 {
+  grid-column-start: 2;
+  grid-column-end: 3;
+  justify-self: left;
+}
+
+.gridItem4 {
+  grid-column-start: 2;
+  grid-column-end: 3;
+  justify-self: left;
+  text-align: left;
+}
+
+.gridItem5 {
+  grid-column-start: 3;
+  grid-column-end: 4;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  justify-self: right;
+}
+
+.gridItem6 {
+  grid-column-start: 3;
+  grid-column-end: 4;
+  grid-row-start: 2;
+  grid-row-end: 3;
+  justify-self: right;
 }
 </style>
