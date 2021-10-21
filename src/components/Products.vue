c5ca139739e9c68ea780604fb92a07609db7fa1e
vanessawa <81361169+vanessawa@users.noreply.github.com>
2021-10-21 20:05:56 +0200

    style: add bootstrap

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,23 +1,34 @@
 <template>
-  <article>
+  <article class="container">
     <div
-      class="prodDiv gridItem1"
+      class="row row-cols-1 row-cols-md-3 p-2 mb-4 bg-light bg-gradient"
       v-for="product in products"
       :key="product.id"
     >
-      <img
-        class="prodPic"
-        :src="require('@/assets/' + product.id + '.jpeg')"
-        alt="gear"
-      />
-      <p class="gridItem2">{{ product.title }}</p>
-      <p class="gridItem3">{{ product.manufacturer }}</p>
-      <p class="gridItem4">{{ product.description }}</p>
-      <p class="gridItem5" v-text="this.parsePrice(product.netPrice)"></p>
-      <p class="gridItem6" v-text="this.parsePrice(product.grossPrice)"></p>
-      <span>incl. VAT</span>
-      <button class="cartBtn" @click="addToCart(product)">Add to cart</button>
-      <p class="gridItem7" v-text="cartQuantity(product)"></p>
+      <div class="col-12 col-md-2">
+        <img
+          class="img img-fluid mb-2"
+          src="https://picsum.photos/200/200"
+          alt="gear"
+        />
+      </div>
+      <div class="col-12 col-md-8">
+        <p>{{ product.title }}</p>
+        <p>{{ product.manufacturer }}</p>
+        <p>{{ product.description }}</p>
+      </div>
+      <div class="col-12 col-md-2">
+        <p v-text="this.parsePrice(product.netPrice)"></p>
+        <p>
+          <span v-text="this.parsePrice(product.grossPrice)"></span>
+          <span> incl. VAT</span>
+        </p>
+
+        <button class="btn btn-sm btn-secondary" @click="addToCart(product)">
+          Add to cart
+        </button>
+        <p v-text="cartQuantity(product)"></p>
+      </div>
     </div>
   </article>
 </template>
@@ -53,11 +64,6 @@ export default {
       if (quantity && quantity.length >= 1)
         return quantity.length + "x in cart";
     },
-    storeToLocalStorage() {
-      const input = this.cart;
-      localStorage.setItem("item", JSON.stringify(input));
-      const output = JSON.parse(localStorage.getItem("item"));
-    },
   },
 
   mounted() {
@@ -68,101 +74,101 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.prodPic {
-  height: 6rem;
-  width: 6rem;
-  background-size: contain;
-  position: absolute;
-}
+// .prodPic {
+//   height: 6rem;
+//   width: 6rem;
+//   background-size: contain;
+//   position: absolute;
+// }
 
-.prodDiv {
-  border: 2px solid black;
-  display: grid;
-  grid-template-columns: 1fr 2fr 2fr;
-  grid-template-rows: 5ex 5ex auto;
-  padding: 1.5rem;
-  grid-gap: 0.3rem;
-  justify-content: flex-start;
-  align-items: flex-start;
-}
+// .prodDiv {
+//   border: 2px solid black;
+//   display: grid;
+//   grid-template-columns: 1fr 2fr 2fr;
+//   grid-template-rows: 5ex 5ex auto;
+//   padding: 1.5rem;
+//   grid-gap: 0.3rem;
+//   justify-content: flex-start;
+//   align-items: flex-start;
+// }
 
-.gridItem1 {
-  grid-column-start: 1;
-  grid-column-end: 2;
-  grid-row-start: 1;
-  grid-row-end: 4;
-  justify-content: flex-start;
-  align-self: flex-start;
-}
+// .gridItem1 {
+//   grid-column-start: 1;
+//   grid-column-end: 2;
+//   grid-row-start: 1;
+//   grid-row-end: 4;
+//   justify-content: flex-start;
+//   align-self: flex-start;
+// }
 
-.gridItem2 {
-  grid-column-start: 2;
-  grid-column-end: 3;
-  justify-self: left;
-}
+// .gridItem2 {
+//   grid-column-start: 2;
+//   grid-column-end: 3;
+//   justify-self: left;
+// }
 
-.gridItem3 {
-  grid-column-start: 2;
-  grid-column-end: 3;
-  justify-self: left;
-}
+// .gridItem3 {
+//   grid-column-start: 2;
+//   grid-column-end: 3;
+//   justify-self: left;
+// }
 
-.gridItem4 {
-  grid-column-start: 2;
-  grid-column-end: 3;
-  grid-row-start: 3;
-  grid-row-end: 5;
-  justify-self: left;
-  text-align: left;
-}
+// .gridItem4 {
+//   grid-column-start: 2;
+//   grid-column-end: 3;
+//   grid-row-start: 3;
+//   grid-row-end: 5;
+//   justify-self: left;
+//   text-align: left;
+// }
 
-.gridItem5 {
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 1;
-  grid-row-end: 2;
-  justify-self: right;
-}
+// .gridItem5 {
+//   grid-column-start: 3;
+//   grid-column-end: 4;
+//   grid-row-start: 1;
+//   grid-row-end: 2;
+//   justify-self: right;
+// }
 
-.gridItem6 {
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 2;
-  grid-row-end: 3;
-  justify-self: right;
-}
+// .gridItem6 {
+//   grid-column-start: 3;
+//   grid-column-end: 4;
+//   grid-row-start: 2;
+//   grid-row-end: 3;
+//   justify-self: right;
+// }
 
-.cartBtn {
-  height: 1.5rem;
-  width: 5.5rem;
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 3;
-  grid-row-end: 4;
-  justify-self: flex-end;
-  align-self: flex-end;
-}
-.gridItem7 {
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 4;
-  grid-row-end: 5;
-  justify-self: flex-end;
-  align-self: flex-end;
-}
+// .cartBtn {
+//   height: 1.5rem;
+//   width: 5.5rem;
+//   grid-column-start: 3;
+//   grid-column-end: 4;
+//   grid-row-start: 3;
+//   grid-row-end: 4;
+//   justify-self: flex-end;
+//   align-self: flex-end;
+// }
+// .gridItem7 {
+//   grid-column-start: 3;
+//   grid-column-end: 4;
+//   grid-row-start: 4;
+//   grid-row-end: 5;
+//   justify-self: flex-end;
+//   align-self: flex-end;
+// }
 
-@media (max-width: 670px) {
-  .prodDiv {
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    grid-template-rows: auto 1ex 2ex auto 2ex 2ex 2ex 2ex;
-  }
-  .prodPic {
-    height: 6rem;
-    width: 6rem;
-    background-size: contain;
-    position: relative;
-  }
-}
+// @media (max-width: 670px) {
+//   .prodDiv {
+//     display: flex;
+//     flex-direction: column;
+//     position: relative;
+//     grid-template-rows: auto 1ex 2ex auto 2ex 2ex 2ex 2ex;
+//   }
+//   .prodPic {
+//     height: 6rem;
+//     width: 6rem;
+//     background-size: contain;
+//     position: relative;
+//   }
+// }
 </style>
